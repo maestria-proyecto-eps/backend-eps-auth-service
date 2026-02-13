a0c7835a85a103d91eba66ddb514ff6df3b4b169
gyuheeJo <davidgyuhee@gmail.com>
2026-02-13 10:08:26 -0500

    boiler plate codes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from core.logger import setup_logging, get_logger
 
 app = FastAPI(
     title="EPS API 2",
@@ -17,9 +18,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+setup_logging()
+
+# Logger call example
+#logger = get_logger(__name__)
+
 @app.get("/")
 def root():
     """Root endpoint"""
+    #logger.info("Root endpoint called")
     return {
         "message": "EPS API",
         "features": [
