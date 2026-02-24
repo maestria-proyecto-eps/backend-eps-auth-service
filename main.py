aa316be0a0df6ffd2adca8465ac85e851873e55d
Rack99 <raulantoniocelyramirez@gmail.com>
2026-02-24 07:12:16 -0500

    Fixed main reverted changes made by mistake

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from fastapi import FastAPI, HTTPException, status , Depends
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from core.logger import setup_logging, get_logger
+
+# Rutas de loggin
+from routers import auth
+
+# Importaciones de Base de Datos
+from db.session import SessionLocal, engine, Base
+from models.user import User
+Base.metadata.create_all(bind=engine)
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="EPS API 2",
@@ -35,10 +47,46 @@ def root():
         "docs": "/docs",
         "redoc": "/redoc"
     }
+#Base de datos de prueba
 
-@app.get("/health")
-def root():
-    """health endpoint"""
-    return {
-        "message": "ok"
-    }
+# CARGA INICIAL DE USUARIOS (SEED)
+def seed_users():
+    db = SessionLocal()
+    try:
+        # Verificamos si ya existen usuarios para no duplicarlos
+        user_count = db.query(User).count()
+        if user_count == 0:
+            print("Base de Datos: No se encontraron usuarios. Cargando datos de prueba...")
+
+            usuarios_prueba = [
+                User(
+                    username="oscar_admin",
+                    password="EPS_2026_Clave",
+                    id_rol=1,
+                    estado=1
+                ),
+                User(
+                    username="manuel",
+                    password="paciente123",
+                    id_rol=2,
+                    estado=0
+                )
+            ]
+
+            db.add_all(usuarios_prueba)
+            db.commit()
+            print("Base de Datos: Usuarios 'oscar_admin' y 'paco_paciente' creados exitosamente.")
+        else:
+            print(f"Base de Datos: Ya existen {user_count} usuarios. Seed omitido.")
+
+    except Exception as e:
+        print(f"Error en seed_users: {e}")
+        db.rollback()
+    finally:
+        db.close()
+
+# Ejecutamos la carga al arrancar el servidor
+seed_users()
+
+# rutas de autenticación
+app.include_router(auth.router)
