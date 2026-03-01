97a5015b52f2da61c00cddcda77ede3e0e2da8ca
gyuheeJo <109032414+gyuheeJo@users.noreply.github.com>
2026-03-01 12:47:14 -0500

    Update session.py

--- db/session.py
+++ db/session.py
@@ -13,6 +13,12 @@ HOST = os.getenv("DB_ADMIN_HOST")
 PORT = os.getenv("DB_ADMIN_PORT")
 DBNAME = os.getenv("DB_ADMIN_NAME")
 
+print(f"USER: {USER}")
+print(f"PASSWORD: {PASSWORD}")
+print(f"HOST: {HOST}")
+print(f"PORT: {PORT}")
+print(f"DBNAME: {DBNAME}")
+      
 # Construcción de la URL con SSL requerido para Supabase
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
